ede18883ee1fe3bd6d202e9c23067707754eaa59
Faniriantsoa RANDRIAHARIMINO <frandriaharimino@yahoo.com>
2024-03-15 19:23:24 +0300

    readonly

--- dist/app.js
+++ dist/app.js
@@ -1,25 +1,6 @@
 "use strict";
-//Extraire un type à partir d'une constante (pas trop utilisé)
-const user1 = {
-    firstname: 'John',
-    lastname: 'Doe',
-    age: 32
-};
-const user = { firstname: "John", lastname: "Doe" };
-function identity(arg) {
-    return arg;
+//Readonly
+function reverse(arr) {
+    return [...arr].reverse();
 }
-const a = identity(3);
-const b = identity(3); //comprend que c'est un nombre même si on ne met pas number
-function first(arg) {
-    return arg[0];
-}
-const aa = first(["aze", "bze", 3]);
-const ab = ["aze", "bze", 3];
-const compteur = document.querySelector('#compteur');
-//l'argument doit obligatoirement avoir un attribut length (les nombres ne marchent pas)
-function consoleSize(arg) {
-    console.log(arg.length);
-    return arg;
-}
-const abb = consoleSize(['3', 2]);
+//Classes
--- src/app.ts
+++ src/app.ts
@@ -1,38 +1,9 @@
-//Alias
-type User = {firstname: string, lastname: string}
-type DateString = string
-type Id = string | number
-type Identity<ArgType> = (arg: ArgType) => ArgType
-type Username = User['firstname']
-
-//Extraire un type à partir d'une constante (pas trop utilisé)
-const user1 = {
-    firstname: 'John',
-    lastname: 'Doe',
-    age: 32
-}
-type User1 = typeof user1
-
-const user: User = {firstname: "John", lastname: "Doe"}
-
-function identity<ArgType>(arg: ArgType): ArgType { //choisir le type de arg une fois et le retour doit être du même type
-    return arg
-}
-const a = identity<number>(3)
-const b = identity(3) //comprend que c'est un nombre même si on ne met pas number
-
-function first<Type>(arg: Type[]): Type {
-    return arg[0]
+//Readonly
+function reverse<T>(arr: readonly T[]): T[] { //la propriété en entrée ne doit pas être modifiée, on doit créer un nouvel objet
+    return [...arr].reverse()
 }
 
-const aa = first(["aze", "bze", 3])
-const ab: Array<string | number> = ["aze", "bze", 3]
-
-const compteur = document.querySelector<HTMLButtonElement>('#compteur')
-
-//l'argument doit obligatoirement avoir un attribut length (les nombres ne marchent pas)
-function consoleSize<Type extends {length: number}>(arg: Type): Type {
-    console.log(arg.length)
-    return arg
+//Classes
+class A {
+    
 }
-const abb = consoleSize(['3', 2])
